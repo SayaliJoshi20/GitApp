a4e1d0e177bf7ee3f693174c70e7e6eab7d39c0a
Lenovo <joshi.sayai2095@gmail.com>
2017-11-20 12:27:49 +0530

    button function is added

--- app/src/main/java/com/example/lenovo/gitapp/MainActivity.java
+++ app/src/main/java/com/example/lenovo/gitapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.lenovo.gitapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hellooo...!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
